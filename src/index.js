57936322906f85a50e49fc2ef86e33f40a557496
Pieter Provoost <pieterprovoost@gmail.com>
2024-05-18 12:46:40 +0200

    analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-ND4LE0X1T0");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
